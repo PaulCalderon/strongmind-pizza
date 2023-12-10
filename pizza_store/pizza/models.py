6fc0c04d0f11d537864345cea878d258b51e281b
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-12-10 18:56:24 +0800

    refactor comments and layout

--- pizza_store/pizza/models.py
+++ pizza_store/pizza/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class PizzaTopping(models.Model):
-    # db_collation is set to make the topping_name unique irregardless of case. 
+    # db_collation is set to make the topping_name unique irregardless of case.
     # might only be applicable to sqlite3 database.
     topping_name = models.CharField(max_length=200, unique=True, error_messages={'unique':'Topping already exists'}, db_collation= 'NOCASE')
 
--- pizza_store/pizza/views.py
+++ pizza_store/pizza/views.py
@@ -9,7 +9,8 @@ from pizza.serializers import PizzaToppingSerializer
 # /
 @api_view(['GET'])
 def homepage(request):
-    "Click on the links below to navigate to different parts of the Pizza Store project"
+    """Click on the links below to navigate to different parts of the Pizza Store project"""
+
     return Response({
         'Topping List' : reverse('toppings_list', request=request)})
 
@@ -22,31 +23,29 @@ class ToppingList(generics.GenericAPIView):
     Only 'owner' users will be able to POST new toppings.<br>
     URLs are included to navigate to individual topping pages.
     """
+
     serializer_class = PizzaToppingSerializer
     queryset = PizzaTopping.objects.all()
     permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
-    # returns a json of toppings
-    # needs to implement a custom serializer to only display the toppings themselves
+    # needs to implement a custom serializer to only display the toppings themselves. Currently returns a json of topings
     def get(self, request):
-        """
-        Returns a list of all pizza toppings
-        """
-        # breakpoint()
+        """Returns a list of all pizza toppings."""
         topping_list = self.get_queryset()
         serializer = PizzaToppingSerializer(topping_list, many=True, context={'request': request})
+
         return Response(serializer.data)
 
     def post(self, request):
-        """
-        Submits a new pizza topping. Must be Unique.
-        """
+        """Submits a new pizza topping. Must be unique."""
         new_topping = request.data
         serializer = PizzaToppingSerializer(data=new_topping, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# toppings/<int:pk>
 class ToppingDetails(generics.GenericAPIView):
     """
     Displays an individual topping<br>
@@ -54,6 +53,7 @@ class ToppingDetails(generics.GenericAPIView):
     Non authenticated users will only be able to use GET.<br>
     Only 'owner' users will be able to edit and delete toppings.
     """
+
     serializer_class = PizzaToppingSerializer
     queryset = PizzaTopping.objects.all()
     permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
@@ -65,19 +65,16 @@ class ToppingDetails(generics.GenericAPIView):
 
         except PizzaTopping.DoesNotExist:
             raise Http404
-        
+
     def get(self, request, pk):
-        """
-        Returns an individual topping
-        """
+        """Returns an individual topping."""
         topping = self._get_object(pk=pk)
         serializer = PizzaToppingSerializer(topping, context={'request': request})
+
         return Response(serializer.data)
 
     def put(self, request, pk):
-        """
-        Updates the currently viewed topping
-        """
+        """Updates the currently viewed topping."""
         topping = self._get_object(pk=pk)
         serializer = PizzaToppingSerializer(topping,  data=request.data, partial=True, context= {'request':request})
         if serializer.is_valid():
@@ -87,9 +84,8 @@ class ToppingDetails(generics.GenericAPIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, pk):
-        """
-        Deletes the currently viewed topping
-        """
+        """Deletes the currently viewed topping."""
         topping = self._get_object(pk=pk)
         topping.delete()
+
         return Response("Sucessfully Deleted", status=status.HTTP_204_NO_CONTENT)
