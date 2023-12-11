8d9fd5037240ca6143676fb003dc932fc832da3e
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-12-11 15:08:45 +0800

    define pizza model and serializer

--- pizza_store/pizza/models.py
+++ pizza_store/pizza/models.py
@@ -3,10 +3,31 @@ from django.db import models
 # Create your models here.
 
 class PizzaTopping(models.Model):
+    """
+    definition
+    """
     # db_collation is set to make the topping_name unique irregardless of case.
     # might only be applicable to sqlite3 database.
-    topping_name = models.CharField(max_length=200, unique=True, error_messages={'unique':'Topping already exists'}, db_collation= 'NOCASE')
+    topping = models.CharField(max_length=200,
+                                    unique=True,
+                                    error_messages={'unique':'Topping already exists'},
+                                    db_collation= 'NOCASE') #rename to topping
+
 
     #TODO can hyperlink to pizzas using it
     def __str__(self):
-        return str(self.topping_name)
+        return str(self.topping)
+
+class Pizza(models.Model):
+    """
+    definition
+    """
+    pizza = models.CharField(max_length=200,
+                                  unique=True,
+                                  error_messages={'unique':'Pizza already exists'},
+                                  db_collation= 'NOCASE') # rename to pizza
+    # size = choicesfield here
+    toppings = models.ManyToManyField(PizzaTopping)
+
+    def __str__(self):
+        return str(self.pizza)
--- pizza_store/pizza/serializers.py
+++ pizza_store/pizza/serializers.py
@@ -1,10 +1,28 @@
-from pizza.models import PizzaTopping
+from pizza.models import PizzaTopping, Pizza
 from rest_framework import serializers
 
 class PizzaToppingSerializer(serializers.ModelSerializer):
+    """
+    Displays toppings and url to individual topping pages
+    """
     # url for individual pizza topping entry
-    url = serializers.HyperlinkedIdentityField(view_name='toppings_detail', read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name='toppings_detail',
+                                               read_only=True)
 
     class Meta:
         model = PizzaTopping
-        fields = ['topping_name', 'url']
+        fields = ['topping', 'url']
+
+
+class PizzaSerializer(serializers.ModelSerializer):
+    """
+    Displays pizza and associated toppings
+    """
+    # SlugRelatedField was used to represent field as 'topping' instead of pk
+    toppings = serializers.SlugRelatedField(many=True,
+                                            queryset= PizzaTopping.objects.all(),
+                                            slug_field='topping')
+
+    class Meta:
+        model = Pizza
+        fields = ['pizza', 'toppings']
