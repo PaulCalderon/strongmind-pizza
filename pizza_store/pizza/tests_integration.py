f4afa5223e72e0d9afcaac81519803d3904402b0
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-12-17 02:11:04 +0800

    create new test for owner permissions. add 401 assert to existing tests

--- pizza_store/pizza/tests_integration.py
+++ pizza_store/pizza/tests_integration.py
@@ -4,8 +4,6 @@ from rest_framework.test import APIClient
 from pizza.models import Pizza, PizzaTopping
 
 
-
-
 class TestHomepage(TestCase):
     def test_get_request_on_homepage_should_return_urls_to_pizza_and_topping(self):
         response = self.client.get('/')
@@ -24,11 +22,16 @@ class TestHomepage(TestCase):
         response = self.client.post('/')
         self.assertContains(response, status_code=405, text='Method \\"POST\\" not allowed')
 
+
 class TestPizzaToppingEndpoints(TestCase):
+    """Tests endpoints located at /toppings/ and /toppings/<int:pk>"""
+
     @classmethod
     def setUpTestData(cls):
-        cls.owner_user = User.objects.create_user(username='owner', password='pass')
-        cls.chef_user = User.objects.create_user(username='chef', password='pass')
+        cls.owner_username = 'owner_created'
+        cls.chef_username = 'chef_created'
+        cls.owner_user = User.objects.create_user(username=cls.owner_username, password='pass')
+        cls.chef_user = User.objects.create_user(username=cls.chef_username, password='pass')
         chef_group, created = Group.objects.get_or_create(name='Pizza Chef')
         owner_group, created = Group.objects.get_or_create(name='Pizza Owner')
 
@@ -54,6 +57,12 @@ class TestPizzaToppingEndpoints(TestCase):
         PizzaTopping.objects.create(topping='Pepperoni')
         PizzaTopping.objects.create(topping='Bacon')
         PizzaTopping.objects.create(topping='Onion')
+    
+    def test_owner_should_have_full_permissions_to_make_changes_to_topping(self):
+        self.assertTrue(self.owner_user.has_perm('pizza.view_pizzatopping'))
+        self.assertTrue(self.owner_user.has_perm('pizza.add_pizzatopping'))
+        self.assertTrue(self.owner_user.has_perm('pizza.change_pizzatopping'))
+        self.assertTrue(self.owner_user.has_perm('pizza.delete_pizzatopping'))
 
     def test_get_should_return_list_of_all_toppings(self):
         response = self.client.get('/toppings/')
@@ -62,12 +71,6 @@ class TestPizzaToppingEndpoints(TestCase):
         self.assertEqual(response.data[0]['topping'], 'Pepperoni')
         self.assertEqual(response.data[1]['topping'], 'Bacon')
         self.assertEqual(response.data[2]['topping'], 'Onion')
-    
-    def test_owner_should_have_full_permissions_to_make_changes_to_topping(self):
-        self.assertTrue(self.owner_user.has_perm('pizza.view_pizzatopping'))
-        self.assertTrue(self.owner_user.has_perm('pizza.add_pizzatopping'))
-        self.assertTrue(self.owner_user.has_perm('pizza.change_pizzatopping'))
-        self.assertTrue(self.owner_user.has_perm('pizza.delete_pizzatopping'))
 
     def test_get_return_empty_list_if_database_is_empty(self):
         PizzaTopping.objects.all().delete()
@@ -79,13 +82,18 @@ class TestPizzaToppingEndpoints(TestCase):
 
     def test_post_should_be_able_to_add_new_toppings_if_user_has_permission(self):
         post_data = {'topping' : 'Ham'}
+
+        # Expected to fail. User is not logged in
+        response = self.client.post('/toppings/', data=post_data)
+        self.assertContains(response, status_code=401, text='Authentication credentials were not provided.')
+
         # Expected to fail. Logged in user does not have permission
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.post('/toppings/', data=post_data)
         self.assertContains(response, status_code=403, text='You do not have permission to perform this action.')
 
         # Expected to succeed
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.post('/toppings/', data=post_data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data['topping'], 'Ham')
@@ -97,7 +105,7 @@ class TestPizzaToppingEndpoints(TestCase):
     def test_post_should_validate_input_and_return_error_if_request_is_invalid(self):
         """topping field is required for all toppings"""
         post_data = {'None existing field' : 'Bacon'}
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.post('/toppings/', data=post_data)
 
         self.assertContains(response, status_code=400, text='This field is required')
@@ -121,13 +129,17 @@ class TestPizzaToppingEndpoints(TestCase):
         stored_toppings = PizzaTopping.objects.all()
         self.assertNotEqual(stored_toppings.values()[1]['topping'], 'Ham')
 
+        # Expected to fail. User is not logged in
+        response = self.client.put('/toppings/2', data=post_data, content_type='application/json')
+        self.assertContains(response, status_code=401, text='Authentication credentials were not provided.')
+
         # Expected to fail. Logged in user does not have permission
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.put('/toppings/2', data=post_data, content_type='application/json')
         self.assertContains(response, status_code=403, text='You do not have permission to perform this action.')
 
         # Expected to succeed
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.put('/toppings/2', data=post_data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['topping'], 'Ham')
@@ -138,7 +150,7 @@ class TestPizzaToppingEndpoints(TestCase):
 
     def test_put_should_validate_input_and_return_error_if_request_is_invalid(self):
         put_data = {'None existing field' : 'Ham'}
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.put('/toppings/2', data=put_data, content_type='application/json')
 
         self.assertContains(response, status_code=400, text='This field is required')
@@ -149,13 +161,17 @@ class TestPizzaToppingEndpoints(TestCase):
         stored_toppings = PizzaTopping.objects.all()
         self.assertEqual(stored_toppings.values()[1]['topping'], 'Bacon')
 
+        # Expected to fail. user is not logged in.
+        response = self.client.delete('/toppings/2', content_type='application/json')
+        self.assertContains(response, status_code=401, text='Authentication credentials were not provided.')
+
         # Expected to fail. Logged in user does not have permission
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.delete('/toppings/2', content_type='application/json')
         self.assertContains(response, status_code=403, text='You do not have permission to perform this action.')
 
         # Expected to succeed
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.delete('/toppings/2', content_type='application/json')
         self.assertEqual(response.status_code, 204)
         self.assertEqual(response.data, 'Sucessfully Deleted')
@@ -169,7 +185,7 @@ class TestPizzaToppingEndpoints(TestCase):
 
     def test_duplicate_toppings_should_not_be_allowed(self):
         """POST and PUT should not allow duplicate toppings"""
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         duplicate_topping = {'topping' : 'Bacon'}
         response = self.client.post('/toppings/', data=duplicate_topping)
         self.assertContains(response, status_code=400, text='Topping already exists')
@@ -179,7 +195,7 @@ class TestPizzaToppingEndpoints(TestCase):
 
     def test_view_should_be_limited_to_implemented_methods_otherwise_raise_405(self):
         """Trying to send an unimplemented method will return a 405"""
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         topping_data = {'topping', 'Ham'}
         response = self.client.put('/toppings/', data=topping_data, content_type='application/json')
         self.assertContains(response, status_code=405, text='Method \\"PUT\\" not allowed')
@@ -193,7 +209,7 @@ class TestPizzaToppingEndpoints(TestCase):
     def test_trying_to_access_a_topping_with_no_entry_should_return_404(self):
         """_get_object method of PizzaToppingDetails should raise Http404"""
         topping_data = {'topping' : 'Ham'}
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.get('/toppings/4')
         self.assertContains(response, status_code=404, text='Not found.')
 
@@ -203,11 +219,16 @@ class TestPizzaToppingEndpoints(TestCase):
         response = self.client.delete('/toppings/4')
         self.assertContains(response, status_code=404, text='Not found.')
 
+
 class TestPizzaEndpoints(TestCase):
+    """Tests endpoints located at /pizzas/ and /pizzas/<int:pk>"""
+
     @classmethod
     def setUpTestData(cls):
-        cls.owner_user = User.objects.create_user(username='owner', password='pass')
-        cls.chef_user = User.objects.create_user(username='chef', password='pass')
+        cls.owner_username = 'owner_created'
+        cls.chef_username = 'chef_created'
+        cls.owner_user = User.objects.create_user(username=cls.owner_username, password='pass')
+        cls.chef_user = User.objects.create_user(username=cls.chef_username, password='pass')
         chef_group, created = Group.objects.get_or_create(name='Pizza Chef')
         owner_group, created = Group.objects.get_or_create(name='Pizza Owner')
 
@@ -250,10 +271,8 @@ class TestPizzaEndpoints(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data[0]['pizza'], 'Pepperoni Pizza')
         self.assertEqual(response.data[0]['toppings'], ['Pepperoni'])
-        self.assertEqual(response.data[0]['url'], 'http://testserver/pizzas/1')
         self.assertEqual(response.data[1]['pizza'], 'Bacon and Onion Pizza')
         self.assertEqual(response.data[1]['toppings'], ['Bacon', 'Onion'])
-        self.assertEqual(response.data[1]['url'], 'http://testserver/pizzas/2')
 
     def test_get_return_empty_list_if_database_is_empty(self):
         Pizza.objects.all().delete()
@@ -265,18 +284,23 @@ class TestPizzaEndpoints(TestCase):
 
     def test_post_should_be_able_to_add_new_pizza_if_user_has_permission(self):
         post_data = {'pizza' : 'All Meat Pizza', 'toppings' : ['Pepperoni', 'Bacon']}
+
+        # Expected to fail. User is not logged in.
+        response = self.client.post('/pizzas/', data=post_data)
+        self.assertContains(response, status_code=401, text='Authentication credentials were not provided.')
+
         # Expected to fail. Logged in user does not have permission
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.post('/pizzas/', data=post_data)
         self.assertContains(response, status_code=403, text='You do not have permission to perform this action.')
 
         # Expected to succeed
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.post('/pizzas/', data=post_data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data['pizza'], 'All Meat Pizza')
         self.assertEqual(response.data['toppings'], ['Pepperoni', 'Bacon'])
-        
+
         # check that All Meat Pizza and associated toppings was saved to the database
         stored_pizzas = Pizza.objects.all()
         stored_pizzas_toppings = stored_pizzas[2].toppings.all()
@@ -288,7 +312,7 @@ class TestPizzaEndpoints(TestCase):
     def test_post_should_validate_input_and_return_error_if_request_is_invalid(self):
         """pizza field is required for all pizzas"""
         post_data = {'None existing field' : 'All Meat Pizza', 'toppings' : ['Pepperoni', 'Bacon']}
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.post('/pizzas/', data=post_data)
 
         self.assertContains(response, status_code=400, text='This field is required')
@@ -303,7 +327,7 @@ class TestPizzaEndpoints(TestCase):
         self.assertEqual(response_bacon_and_onion_pizza.status_code, 200)
         self.assertDictContainsSubset({'pizza' : 'Bacon and Onion Pizza'}, response_bacon_and_onion_pizza.data) 
         self.assertDictContainsSubset({'toppings' : ['Bacon', 'Onion']}, response_bacon_and_onion_pizza.data)
-    
+
     def test_put_should_be_able_to_update_a_pizza_if_user_has_permission(self):
         """PUT should change Bacon and Onion pizza to Pepperoni and Onion Pizza and save to database"""
         put_data = {'pizza' : 'Pepperoni and Onion Pizza', 'toppings' : ['Pepperoni', 'Onion']}
@@ -312,12 +336,16 @@ class TestPizzaEndpoints(TestCase):
         self.assertNotEqual(stored_pizzas.values()[1]['pizza'], 'Pepperoni and Onion Pizza')
 
         # Expected to fail. Logged in user does not have permission
-        self.client.login(username='owner', password='pass')
+        response = self.client.put('/pizzas/1', data=put_data, content_type='application/json')
+        self.assertContains(response, status_code=401, text='Authentication credentials were not provided.')
+
+        # Expected to fail. Logged in user does not have permission
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.put('/pizzas/1', data=put_data, content_type='application/json')
         self.assertContains(response, status_code=403, text='You do not have permission to perform this action.')
 
         # Expected to succeed
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.put('/pizzas/2', data=put_data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['pizza'], 'Pepperoni and Onion Pizza')
@@ -332,7 +360,7 @@ class TestPizzaEndpoints(TestCase):
 
     def test_put_should_validate_input_and_return_error_if_request_is_invalid(self):
         put_data = {'None existing field' : 'Pepperoni and Onion Pizza', 'toppings' : ['Pepperoni', 'Onion']}
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.put('/pizzas/1', data=put_data, content_type='application/json')
 
         self.assertContains(response, status_code=400, text='This field is required')
@@ -343,13 +371,17 @@ class TestPizzaEndpoints(TestCase):
         stored_pizza = Pizza.objects.all()
         self.assertEqual(stored_pizza.values()[0]['pizza'], 'Pepperoni Pizza')
 
+        # Expected to fail. User is not logged in.
+        response = self.client.delete('/pizzas/1', content_type='application/json')
+        self.assertContains(response, status_code=401, text='Authentication credentials were not provided.')
+
         # Expected to fail. Logged in user does not have permission
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.delete('/pizzas/1', content_type='application/json')
         self.assertContains(response, status_code=403, text='You do not have permission to perform this action.')
 
         # Expected to succeed
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.delete('/pizzas/1', content_type='application/json')
         self.assertEqual(response.status_code, 204)
         self.assertEqual(response.data, 'Sucessfully Deleted')
@@ -363,7 +395,7 @@ class TestPizzaEndpoints(TestCase):
 
     def test_duplicate_pizzas_should_not_be_allowed(self):
         """POST and PUT should not allow duplicate toppings"""
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         duplicate_pizza = {'pizza' : 'Pepperoni Pizza', 'toppings' : ['Pepperoni']}
         response = self.client.post('/pizzas/', data=duplicate_pizza)
         self.assertContains(response, status_code=400, text='Pizza already exists')
@@ -373,7 +405,7 @@ class TestPizzaEndpoints(TestCase):
 
     def test_view_should_be_limited_to_implemented_methods_otherwise_raise_405(self):
         """Trying to send an unimplemented method will return a 405"""
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         pizza_data = {'pizza' : 'All Meat Pizza', 'toppings' : ['Pepperoni', 'Bacon']}
         response = self.client.put('/pizzas/', data=pizza_data, content_type='application/json')
         self.assertContains(response, status_code=405, text='Method \\"PUT\\" not allowed')
@@ -387,7 +419,7 @@ class TestPizzaEndpoints(TestCase):
     def test_trying_to_access_a_pizza_with_no_entry_should_return_404(self):
         """_get_object method of PizzaDetails should raise Http404"""
         pizza_data = {'pizza' : 'All Meat Pizza', 'toppings' : ['Pepperoni', 'Bacon']}
-        self.client.login(username='chef', password='pass')
+        self.client.login(username=self.chef_username, password='pass')
         response = self.client.get('/pizzas/3')
         self.assertContains(response, status_code=404, text='Not found.')
 
@@ -406,7 +438,7 @@ class TestPizzaEndpoints(TestCase):
         self.assertEqual(stored_pizzas_toppings.values()[0]['topping'], 'Bacon')
         self.assertEqual(stored_pizzas_toppings.values()[1]['topping'], 'Onion')
 
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.put('/toppings/2', data=new_topping, content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
@@ -426,7 +458,7 @@ class TestPizzaEndpoints(TestCase):
         self.assertEqual(stored_pizzas_toppings.values()[1]['topping'], 'Onion')
         self.assertEqual(len(stored_pizzas_toppings.values()), 2)
 
-        self.client.login(username='owner', password='pass')
+        self.client.login(username=self.owner_username, password='pass')
         response = self.client.delete('/toppings/2')
         self.assertEqual(response.status_code, 204)
 
@@ -436,6 +468,4 @@ class TestPizzaEndpoints(TestCase):
 
         self.assertEqual(stored_pizzas_toppings.values()[0]['topping'], 'Onion')
         self.assertEqual(len(stored_pizzas_toppings.values()), 1)
-        
-
 
