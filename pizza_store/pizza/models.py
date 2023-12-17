40ab46fc76cc9d7707d80169cc2aa5f6c1b5c008
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-12-17 17:53:17 +0800

    adjust comments and code structure

--- pizza_store/pizza/models.py
+++ pizza_store/pizza/models.py
@@ -1,32 +1,28 @@
 from django.db import models
 
-# Create your models here.
 
-class PizzaTopping(models.Model):
-    """
-    definition
-    """
-    # db_collation is set to make the topping_name unique irregardless of case.
+class PizzaTopping(models.Model): # TODO Change to Topping
+    """Models a single PizzaTopping instance"""
+
+    # db_collation is set to make the topping unique irregardless of case.
     # might only be applicable to sqlite3 database.
-    topping = models.CharField(max_length=200,
-                                    unique=True,
-                                    error_messages={'unique':'Topping already exists'},
-                                    db_collation= 'NOCASE') #rename to topping
-
-
-    #TODO can hyperlink to pizzas using it
+    topping = models.CharField(
+                            max_length=200, unique=True,
+                            error_messages={'unique':'Topping already exists'},
+                            db_collation= 'NOCASE'
+                            )
     def __str__(self):
         return str(self.topping)
 
+
 class Pizza(models.Model):
-    """
-    definition
-    """
-    pizza = models.CharField(max_length=200,
-                                  unique=True,
-                                  error_messages={'unique':'Pizza already exists'},
-                                  db_collation= 'NOCASE') # rename to pizza
-    # size = choicesfield here
+    """Models a single Pizza instance"""
+
+    pizza = models.CharField(
+                            max_length=200, unique=True,
+                            error_messages={'unique':'Pizza already exists'},
+                            db_collation= 'NOCASE'
+                            )
     toppings = models.ManyToManyField(PizzaTopping)
 
     def __str__(self):
--- pizza_store/pizza/serializers.py
+++ pizza_store/pizza/serializers.py
@@ -2,12 +2,13 @@ from pizza.models import PizzaTopping, Pizza
 from rest_framework import serializers
 
 class PizzaToppingSerializer(serializers.ModelSerializer):
-    """
-    Displays toppings and url to individual topping pages
-    """
+    """Displays toppings and url to individual topping pages."""
+
     # url for individual pizza topping entry
-    url = serializers.HyperlinkedIdentityField(view_name='toppings_detail',
-                                               read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+                                            view_name='toppings_detail',
+                                            read_only=True
+                                            )
 
     class Meta:
         model = PizzaTopping
@@ -15,14 +16,20 @@ class PizzaToppingSerializer(serializers.ModelSerializer):
 
 
 class PizzaSerializer(serializers.ModelSerializer):
-    """
-    Displays pizza and associated toppings
-    """
-    # SlugRelatedField was used to represent field as 'topping' instead of pk
-    toppings = serializers.SlugRelatedField(many=True,
-                                            queryset= PizzaTopping.objects.all(),
-                                            slug_field='topping')
+    """Displays pizza, associated toppings and url to individual pizza pages."""
 
+    # SlugRelatedField was used to represent field as 'topping' instead of pk
+    toppings = serializers.SlugRelatedField(
+                                            many=True,
+                                            queryset= PizzaTopping.objects.all(),
+                                            slug_field='topping'
+                                            )
+                                            
+    url = serializers.HyperlinkedIdentityField(
+                                            view_name='pizzas_detail',
+                                            read_only=True
+                                            )
+    
     class Meta:
         model = Pizza
-        fields = ['pizza', 'toppings']
+        fields = ['pizza', 'toppings', 'url' ]
